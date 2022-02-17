ad41e2812fc7538c4484c8972c37b71a2e3ad46a
AndrewJamson24 <63559804+AndrewJamson24@users.noreply.github.com>
2022-02-17 11:11:45 +0200

    Update Laboratorul2MD.py

--- python/discrete_math/Laboratorul2MD.py
+++ python/discrete_math/Laboratorul2MD.py
@@ -1,56 +1,46 @@
 from collections import defaultdict
- 
-lenght = int(input("introduceti numarul de muchii din gaf "))
-root = int(input("Introduceti nodul principal "))
+
+nodes = list(map(int,input("introduceti nodurule prin spatiu " ).split()))
+all_edges = list(tuple(map(int,input().split())) for r in range(int(input('Numarul de muchii si perechile de muchii : '))))
 
 class Graph:
- 
-    # Constructor
-    def __init__(self):
- 
-        # default dictionary to store graph
-        self.graph = defaultdict(list)
- 
-    # function to add an edge to graph
-    def addEdge(self, u, v):
-        self.graph[u].append(v)
- 
-    # A function used by DFS
-    def DFSUtil(self, v, visited):
- 
-        # Mark the current node as visited
-        # and print it
-        visited.add(v)
-        print(v, end=' ')
- 
-        # Recur for all the vertices
-        # adjacent to this vertex
-        for neighbour in self.graph[v]:
-            if neighbour not in visited:
-                self.DFSUtil(neighbour, visited)
- 
-    # The function to do DFS traversal. It uses
-    # recursive DFSUtil()
-    def DFS(self, v):
- 
-        # Create a set to store visited vertices
-        visited = set()
- 
-        # Call the recursive helper function
-        # to print DFS traversal
-        self.DFSUtil(v, visited)
- 
-# Driver code
- 
- 
-# Create a graph given
-# in the above diagram
-g = Graph()
-for x in range(0,lenght):
-    i = int(input("Introduceti primul nod: "))
-    q = int(input("Introduceti nodul conex cu primul: "))
-    g.addEdge(i,q)
-    x += 1    
- 
-print("Following is DFS from (starting from vertex {} )".format(root))
-g.DFS(root)
+        def __init__(self,Nodes):
+            self.graph = defaultdict(list)
+            self.nodes = Nodes
+            self.adj_list = {}
+            for node in self.nodes:
+                self.adj_list[node] = []
+
+        def add_edge(self,u,v):
+            self.adj_list[u].append(v)
+            self.adj_list[v].append(u)
+            self.graph[u].append(v)
+            self.graph[v].append(u)
+
+        def adds_zero(self):
+            for node in self.nodes:
+                self.adj_list[node].append(str(0))
+
+        def print_adj_list(self):
+            for node in self.nodes:
+                print(node,"->",self.adj_list[node])
+        
+        def DFSUtil(self, v, visited):
+            visited.add(v)
+            print(v, end=' ')
+            for neighbour in self.graph[v]:
+                if neighbour not in visited:
+                    self.DFSUtil(neighbour, visited)
+        def DFS(self, v):
+            visited = set()
+            self.DFSUtil(v, visited)
+   
+graph = Graph(nodes)
+for u,v in all_edges:
+    graph.add_edge(u,v)
+graph.adds_zero()
+print("Lista adiacenta")
+graph.print_adj_list()
+root = int(input("Introduceti nodul principal "))
+print("Algoritmul de cautare in adancime cu nodul principal {} ".format(root))
+graph.DFS(root)
