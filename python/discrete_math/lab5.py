f4a00ef673b18645294ab5baa72cf47f821bbd64
AndrewJamson24 <63559804+AndrewJamson24@users.noreply.github.com>
2022-03-04 08:25:31 +0200

    Update lab5.py

--- python/discrete_math/lab5.py
+++ python/discrete_math/lab5.py
@@ -1,17 +1,13 @@
 import networkx as nx
-from networkx.algorithms.flow import edmonds_karp
+
 
 G = nx.DiGraph()
 
 edges_nr = int(input("introduceti numarul de muchii: "))
 
 for x in range(edges_nr):
-        G.add_edge(int(input("Introduceti primnul nod: ")),int(input("introduceti nodul adicent: ")), capacity = float(input("Introduceti ponderea: ")))
+        G.add_edge(int(input("Introduceti primnul nod: ")),int(input("introduceti nodul adicent: ")), capacity = float(input("Introduceti capacitatea: ")))
         x += 1
-
-listnodes = list(G.nodes)
-
-R = edmonds_karp(G, listnodes[0], listnodes[len(listnodes)-1])
 
 x = int(input("introduceti nodul de start: "))
 y = int(input("Introduceti nodul de finis: "))
@@ -21,5 +17,4 @@ while x and y not in G.nodes:
     y = int(input("Introduceti nodul de finis: "))
 
 flow_value = nx.maximum_flow_value(G, x, y)
-flow_value == R.graph["flow_value"]
 print(flow_value)
