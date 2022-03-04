ae646759986e52a544071a4c32a335e2a0ee373d
AndrewJamson24 <63559804+AndrewJamson24@users.noreply.github.com>
2022-03-04 08:39:04 +0200

    Update lab5.py

--- python/discrete_math/lab5.py
+++ python/discrete_math/lab5.py
@@ -1,6 +1,5 @@
 import networkx as nx
 
-
 G = nx.DiGraph()
 
 edges_nr = int(input("introduceti numarul de muchii: "))
@@ -9,12 +8,19 @@ for x in range(edges_nr):
         G.add_edge(int(input("Introduceti primnul nod: ")),int(input("introduceti nodul adicent: ")), capacity = float(input("Introduceti capacitatea: ")))
         x += 1
 
-x = int(input("introduceti nodul de start: "))
-y = int(input("Introduceti nodul de finis: "))
-while x and y not in G.nodes:
-    print("nodurile nu fac parte din graf")
+while True:
     x = int(input("introduceti nodul de start: "))
     y = int(input("Introduceti nodul de finis: "))
+    while x and y not in G.nodes:
+        print("nodurile nu fac parte din graf")
+        x = int(input("introduceti nodul de start: "))
+        y = int(input("Introduceti nodul de finis: "))
 
-flow_value = nx.maximum_flow_value(G, x, y)
-print(flow_value)
+    flow_value = nx.maximum_flow_value(G, x, y)
+    print("Capacitatea maxima pe graf este {}".format(flow_value))
+    r = input("Doriti capacitatea pentru alt arc?(Da/Nu)").capitalize()
+    if r =="Da":
+        pass
+    else:
+        print("Iesire...")
+        break
