320b423577fc6249f0c8a7751cc32e560845dba4
AndrewJamson24 <63559804+AndrewJamson24@users.noreply.github.com>
2022-03-04 07:50:43 +0200

    Update lab4.py

--- python/discrete_math/lab4.py
+++ python/discrete_math/lab4.py
@@ -1,34 +1,35 @@
 import networkx as nx 
 
-nodes = list(map(int,input("introduceti nodurule prin spatiu " ).split()))
 edges_nr = int(input("introduceti numarul de muchii: "))
 
 G = nx.DiGraph()
 
-while True:
+path_l_s = input("Cel mai lung sau cel mai scurt drum?(lung/scurt): ").lower()
+
+while path_l_s != "lung" or path_l_s != "scurt":
+    print("Introduceti dupa modelul din paranteze (lung/scurt )")
     path_l_s = input("Cel mai lung sau cel mai scurt drum?(lung/scurt): ").lower()
     if path_l_s == "lung":
         for x in range(edges_nr):
             G.add_edge(int(input("Introduceti primnul nod: ")),int(input("introduceti nodul adicent: ")), weight = -abs(int(input("Introduceti ponderea: "))))
             x += 1
-
         length = dict(nx.all_pairs_bellman_ford_path_length(G))
         break
-            
+ 
     elif path_l_s == "scurt":
         for x in range(edges_nr):
             G.add_edge(int(input("Introduceti primul nod: ")),int(input("introduceti nodul adicent: ")), weight = int(input("Introduceti ponderea: ")))
             x += 1
-
         length = dict(nx.all_pairs_bellman_ford_path_length(G))
         break
-
-    else:
-        print("Specificati alegerea dupa exemplul din paranteze")
 
 while True:
     x = int(input("introduceti nodul de start: "))
     y = int(input("Introduceti nodul de finis: "))
+    while x and y not in G.nodes:
+        print("nodurile nu fac parte din graf")
+        x = int(input("introduceti nodul de start: "))
+        y = int(input("Introduceti nodul de finis: "))
     
     if path_l_s == "lung":
         print("drumul maxim din {} in {} ".format(x,y))
@@ -43,6 +44,7 @@ while True:
         pass
     else :
         break
+
 
 
 #Bellman-Ford algortims shortest or longest path in graph
