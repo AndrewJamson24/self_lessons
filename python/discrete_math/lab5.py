88cccffc322e9ac6ee9f4ce30ac47f8219410166
AndrewJamson24 <63559804+AndrewJamson24@users.noreply.github.com>
2022-03-27 12:21:40 +0300

    Update lab5.py

--- python/discrete_math/lab5.py
+++ python/discrete_math/lab5.py
@@ -1,13 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import networkx as nx
 
 G = nx.DiGraph()
 
-edges_nr = int(input("introduceti numarul de muchii: "))
-
-for x in range(edges_nr):
-        G.add_edge(int(input("Introduceti primnul nod: ")),int(input("introduceti nodul adicent: ")), capacity = float(input("Introduceti capacitatea: ")))
-        x += 1
-
+G = nx.read_edgelist("graph5.txt", nodetype=int, data=(("capacity", float),))
+        
 while True:
     x = int(input("introduceti nodul de start: "))
     y = int(input("Introduceti nodul de finis: "))
@@ -18,9 +16,7 @@ while True:
 
     flow_value = nx.maximum_flow(G, x, y)
     print("Capacitatea maxima pe graf este {}".format(flow_value))
-    r = input("Doriti capacitatea pentru alt arc?(Da/Nu)").capitalize()
-    if r =="Da":
-        pass
-    else:
-        print("Iesire...")
-        break
+    
+    break
+
+#Reads from a file in the same directory
